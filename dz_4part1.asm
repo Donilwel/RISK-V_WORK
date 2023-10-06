e876624106d38299efe4bafd6d77f960aa9e51b6
Maslow Danila Yurevich <134236921+Donilwel@users.noreply.github.com>
2023-10-06 22:23:23 +0300

    Максимальное число факториал 32 бит

--- dz_4part1.asm
+++ dz_4part1.asm
@@ -5,34 +5,30 @@
 
 _start:
     li a0, 1          	# start arg
-    li a1, 1          	# start factorial
+    li s1, 1          	# start factorial
     jal factorial     	# uploading factorial
     
-    mv a1, a0
+    mv s1, a0
     li a7, 1
     ecall
 j stop
 
 factorial:
+    li s3, 0        	# change s3 on a max digit
     li a2, 0x7FFFFFFF 	# max 32 digit
-    li t0, 20         	# change a0 on a max digit
-    bge a0, t0, end 	# if a0 > max digit -> stop
-    
-    mv a4, a1    	# this conn in a4
-    mul a1, a1, a0 
-    mulh a3, a4, a0 
-    beqz a3, over 	
-    
+loop:
+    mv s2, s1    	# this conn in s2
+    mul s1, s1, a0 
+    mulh s3, s2, a0 
+    beqz s3, over 	
     addi a0, a0, -1   
-    ret
+    j rett
     
 over:
     addi a0, a0, 1   
-    jal factorial     	# start recursion
-
-end:
+    j loop     	# start recursion
+rett:
     ret
-
 stop:
     li a7, 10
     ecall
