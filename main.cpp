7659b8f9dfa74a298c6870cf752d8b6a2003ff35
Maslow Danila Yurevich <134236921+Donilwel@users.noreply.github.com>
2023-12-22 14:30:38 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,83 +1,14 @@
 #include <iostream>
-#include <thread>
-#include <mutex>
-#include <condition_variable>
-
-std::mutex mtx;
-std::condition_variable cv;
-int available_single_rooms = 10;
-int available_double_rooms = 15;
-
-void guestArrives(const std::string& guest_name, bool is_female) {
-    std::unique_lock<std::mutex> lock(mtx);
-
-    if (is_female) {
-        if (available_single_rooms > 0)
-        {
-            std::cout << guest_name << " (female) has been assigned a single room.\n";
-            available_single_rooms--;
-        }
-        else
-        {
-            std::cout << guest_name << " (female) couldn't find a suitable room and left.\n";
-            return;
-        }
-    }
-    else
-    {
-        if (available_double_rooms > 0) {
-            std::cout << guest_name << " (male) has been assigned a double room.\n";
-            available_double_rooms--;
-        }
-        else
-        {
-            std::cout << guest_name << " (male) couldn't find a suitable room and left.\n";
-            return;
-        }
-    }
-    cv.notify_all();
-}
-
-void guestDeparts(const std::string& guest_name, bool is_female) {
-    std::unique_lock<std::mutex> lock(mtx);
-
-    if (is_female)
-    {
-        available_single_rooms++;
-        std::cout << guest_name << " (female) has departed and the single room is available.\n";
-    }
-    else
-    {
-        available_double_rooms++;
-        std::cout << guest_name << " (male) has departed and the double room is available.\n";
-    }
-
-    cv.notify_all();
-}
-
-void hotelAdministrator() {
-    std::unique_lock<std::mutex> lock(mtx);
-
-    cv.wait(lock, []{ return (available_single_rooms > 0 || available_double_rooms > 0); });
-
-    std::cout << "Hotel administrator thread started.\n";
-
-    while (true) {
-        cv.wait(lock, []{ return (available_single_rooms == 0 || available_double_rooms == 0); });
-
-        if (available_single_rooms == 0) std::cout << "No single rooms available. Waiting for departures...\n";
-        else if (available_double_rooms == 0) std::cout << "No double rooms available. Waiting for departures...\n";
-
-        cv.wait(lock, []{ return (available_single_rooms > 0 || available_double_rooms > 0); });
-    }
-}
+#include <vector>
+#include <string>
+#include <omp.h>
 
 int main() {
-    std::thread administrator_thread(hotelAdministrator);
-
     std::cout << "Welcome to the hotel!\n";
-    std::cout << "Please enter the guest details. Type 'exit' to stop.\n";
+    std::cout << "Please enter guest details. Type 'exit' to stop.\n";
 
+    std::vector<std::string> guest_names;
+    std::vector<std::string> guest_genders;
     std::string guest_name;
     std::string guest_gender;
 
@@ -92,13 +23,26 @@ int main() {
         std::cout << "Guest gender (male/female): ";
         std::getline(std::cin, guest_gender);
 
-        bool is_female = (guest_gender == "female");
-
-        std::thread guest_thread(guestArrives, guest_name, is_female);
-        guest_thread.detach();
+        guest_names.push_back(guest_name);
+        guest_genders.push_back(guest_gender);
     }
 
-    administrator_thread.join();
+#pragma omp parallel for
+    for (int i = 0; i < guest_names.size(); ++i) {
+        std::string guest_name = guest_names[i];
+        std::string guest_gender = guest_genders[i];
+
+#pragma omp critical
+        {
+            if (guest_gender == "female") {
+                std::cout << guest_name << " (female) has been assigned a room.\n";
+            } else if (guest_gender == "male") {
+                std::cout << guest_name << " (male) has been assigned a room.\n";
+            } else {
+                std::cout << "Invalid gender for guest " << guest_name << ". Skipping.\n";
+            }
+        }
+    }
 
     return 0;
 }
